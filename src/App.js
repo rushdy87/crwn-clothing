d993540f3401e13fbda1804139579011dc46c892
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-05-20 13:38:52 +0300

    adding cart context and shoping icon as well as cart dropdwon

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.compnent";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/athentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+// import CartIcon from "./components/cart-icon/cart-icon.component";
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Navigation />}>
       <Route index element={<Home />} />
+      <Route path="shop" element={<Shop />} />
       <Route path="auth" element={<Authentication />} />
+      {/* <Route path="cart" element={<CartIcon />} /> */}
     </Route>
   </Routes>
 );
