5b781d5d0a76441429685507917406377bd5c550
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-05-22 11:26:33 +0300

    Create checkout Page along with checkout items and chechout cart functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navigation from "./routes/navigation/navigation.compnent";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/athentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
-// import CartIcon from "./components/cart-icon/cart-icon.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 const App = () => (
   <Routes>
@@ -12,11 +12,9 @@ const App = () => (
       <Route index element={<Home />} />
       <Route path="shop" element={<Shop />} />
       <Route path="auth" element={<Authentication />} />
-      {/* <Route path="cart" element={<CartIcon />} /> */}
+      <Route path="checkout" element={<Checkout />} />
     </Route>
   </Routes>
 );
 
 export default App;
-
-// crwn-clothing-db-2a774
