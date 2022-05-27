0165d7f5835746bb2889a05030f55602eff3e7b6
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-05-28 00:46:20 +0300

    move from context to Redux, build Store, reducers, actions, selector for categories and cart

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import { CategoriesProvider } from "./contexts/categories.context";
-import { CartProvider } from "./contexts/cart.context";
 import { store } from "./store/store";
 
 import "./index.scss";
@@ -16,11 +14,7 @@ root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <CategoriesProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </CategoriesProvider>
+      <App />
     </BrowserRouter>
   </Provider>
   // </React.StrictMode>
