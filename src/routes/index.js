503f038c59726b5a5e4d568dd212272fb81a8d09
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-03-11 10:21:55 +0300

    React router: BrowserRouter, Rotes, Route, Outlet, Link. starting build navigation. Some styling improve

---
+++ src/routes/index.js
@@ -0,0 +1,2 @@
+export { default as Navigation } from './navigation/Navigation';
+export { default as Home } from './home/Home';
---
+++ src/routes/navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './Navigation.scss';
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link to="/" className="logo-container">
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link to="/shop" className="nav-link">
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
